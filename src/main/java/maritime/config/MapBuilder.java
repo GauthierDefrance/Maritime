5acd24b6dc3dcd0e99a5c06dae8eaaf62a78db28
tartinax <pokemon.furtif@gmail.com>
2025-02-14 15:38:04 +0100

    test

--- src/main/java/maritime/config/MapBuilder.java
+++ src/main/java/maritime/config/MapBuilder.java
@@ -4,6 +4,7 @@ import maritime.engine.entity.*;
 import maritime.engine.faction.Faction;
 import maritime.engine.faction.Player;
 import maritime.engine.graph.GraphPoint;
+import maritime.engine.graph.GraphSegment;
 import maritime.gui.PopUp;
 
 import java.awt.*;
@@ -35,6 +36,10 @@ public class MapBuilder {
             }
         }
     }
+    public MapBuilder(){
+        timeStop = false;
+        init0();
+    }
 
     public void init0(){
         ArrayList<PopUp> lstPopUp =new ArrayList<>();
@@ -43,18 +48,84 @@ public class MapBuilder {
         ArrayList<Faction> lstBotFaction = new ArrayList<>();
         HashMap<String, GraphPoint> mapGraphPoint = new HashMap<>();
         Player player = new Player("blue");
-
 //        map.put("A",new GraphPoint(new Point(0*GameConfiguration.GAME_SCALE,0*GameConfiguration.GAME_SCALE),"A"));
-//        GraphPoint AHarbor = new GraphPoint(new Point(95*GameConfiguration.GAME_SCALE,65*GameConfiguration.GAME_SCALE),"AHarbor");
-//        GraphPoint A = new GraphPoint(new Point(130*GameConfiguration.GAME_SCALE,130*GameConfiguration.GAME_SCALE),"A");
-//        GraphPoint A = new GraphPoint(new Point(225*GameConfiguration.GAME_SCALE,125*GameConfiguration.GAME_SCALE),"A");
-//        GraphPoint A = new GraphPoint(new Point(180*GameConfiguration.GAME_SCALE,90*GameConfiguration.GAME_SCALE),"A");
-//        GraphPoint A = new GraphPoint(new Point(275*GameConfiguration.GAME_SCALE,50*GameConfiguration.GAME_SCALE),"A");
-//        GraphPoint A = new GraphPoint(new Point(65*GameConfiguration.GAME_SCALE,85*GameConfiguration.GAME_SCALE),"A");
-//        GraphPoint A = new GraphPoint(new Point(185*GameConfiguration.GAME_SCALE,235*GameConfiguration.GAME_SCALE),"A");
-//        GraphPoint A = new GraphPoint(new Point(185*GameConfiguration.GAME_SCALE,165*GameConfiguration.GAME_SCALE),"A");
-//        GraphPoint A = new GraphPoint(new Point(205*GameConfiguration.GAME_SCALE,140*GameConfiguration.GAME_SCALE),"A");
+        GraphPoint AHarbor = new GraphPoint(new Point(65*GameConfiguration.GAME_SCALE,80*GameConfiguration.GAME_SCALE),"AHarbor");
+        GraphPoint BHarbor = new GraphPoint(new Point(150*GameConfiguration.GAME_SCALE,300*GameConfiguration.GAME_SCALE),"BHarbor");
+        GraphPoint CHarbor = new GraphPoint(new Point(340*GameConfiguration.GAME_SCALE,130*GameConfiguration.GAME_SCALE),"CHarbor");
+        GraphPoint DHarbor = new GraphPoint(new Point(580*GameConfiguration.GAME_SCALE,225*GameConfiguration.GAME_SCALE),"DHarbor");
 
+        GraphPoint P1 = new GraphPoint(new Point(135*GameConfiguration.GAME_SCALE,125*GameConfiguration.GAME_SCALE),"P1");
+        GraphPoint P2 = new GraphPoint(new Point(255*GameConfiguration.GAME_SCALE,150*GameConfiguration.GAME_SCALE),"P2");
+        GraphPoint P3 = new GraphPoint(new Point(140*GameConfiguration.GAME_SCALE,225*GameConfiguration.GAME_SCALE),"P3");
+        GraphPoint P4 = new GraphPoint(new Point(200*GameConfiguration.GAME_SCALE,240*GameConfiguration.GAME_SCALE),"P4");
+        GraphPoint P5 = new GraphPoint(new Point(340*GameConfiguration.GAME_SCALE,215*GameConfiguration.GAME_SCALE),"P5");
+        GraphPoint P6 = new GraphPoint(new Point(420*GameConfiguration.GAME_SCALE,160*GameConfiguration.GAME_SCALE),"P6");
+        GraphPoint P7 = new GraphPoint(new Point(490*GameConfiguration.GAME_SCALE,205*GameConfiguration.GAME_SCALE),"P7");
+
+        AHarbor.addSegment(new GraphSegment(P1, (int) AHarbor.getPoint().distance(P1.getPoint())));
+
+        BHarbor.addSegment(new GraphSegment(P3, (int) BHarbor.getPoint().distance(P3.getPoint())));
+        BHarbor.addSegment(new GraphSegment(P4, (int) BHarbor.getPoint().distance(P4.getPoint())));
+
+        CHarbor.addSegment(new GraphSegment(P2, (int) CHarbor.getPoint().distance(P2.getPoint())));
+        CHarbor.addSegment(new GraphSegment(P5, (int) CHarbor.getPoint().distance(P5.getPoint())));
+        CHarbor.addSegment(new GraphSegment(P6, (int) CHarbor.getPoint().distance(P6.getPoint())));
+
+        DHarbor.addSegment(new GraphSegment(P7, (int) DHarbor.getPoint().distance(P7.getPoint())));
+
+        P1.addSegment(new GraphSegment(AHarbor, (int) P1.getPoint().distance(AHarbor.getPoint())));
+        P1.addSegment(new GraphSegment(P2, (int) P1.getPoint().distance(P2.getPoint())));
+        P1.addSegment(new GraphSegment(P3, (int) P1.getPoint().distance(P3.getPoint())));
+        P1.addSegment(new GraphSegment(P4, (int) P1.getPoint().distance(P4.getPoint())));
+
+        P2.addSegment(new GraphSegment(CHarbor, (int) P2.getPoint().distance(CHarbor.getPoint())));
+        P2.addSegment(new GraphSegment(P1, (int) P2.getPoint().distance(P1.getPoint())));
+        P2.addSegment(new GraphSegment(P3, (int) P2.getPoint().distance(P3.getPoint())));
+        P2.addSegment(new GraphSegment(P4, (int) P2.getPoint().distance(P4.getPoint())));
+        P2.addSegment(new GraphSegment(P5, (int) P2.getPoint().distance(P5.getPoint())));
+
+        P3.addSegment(new GraphSegment(BHarbor, (int) P3.getPoint().distance(BHarbor.getPoint())));
+        P3.addSegment(new GraphSegment(P1, (int) P3.getPoint().distance(P1.getPoint())));
+        P3.addSegment(new GraphSegment(P2, (int) P3.getPoint().distance(P2.getPoint())));
+        P3.addSegment(new GraphSegment(P4, (int) P3.getPoint().distance(P4.getPoint())));
+
+        P4.addSegment(new GraphSegment(BHarbor, (int) P4.getPoint().distance(BHarbor.getPoint())));
+        P4.addSegment(new GraphSegment(P1, (int) P4.getPoint().distance(P1.getPoint())));
+        P4.addSegment(new GraphSegment(P2, (int) P4.getPoint().distance(P2.getPoint())));
+        P4.addSegment(new GraphSegment(P3, (int) P4.getPoint().distance(P3.getPoint())));
+        P4.addSegment(new GraphSegment(P5, (int) P4.getPoint().distance(P5.getPoint())));
+
+        P5.addSegment(new GraphSegment(CHarbor, (int) P5.getPoint().distance(CHarbor.getPoint())));
+        P5.addSegment(new GraphSegment(P2, (int) P5.getPoint().distance(P2.getPoint())));
+        P5.addSegment(new GraphSegment(P4, (int) P5.getPoint().distance(P4.getPoint())));
+        P5.addSegment(new GraphSegment(P6, (int) P5.getPoint().distance(P6.getPoint())));
+        P5.addSegment(new GraphSegment(P7, (int) P5.getPoint().distance(P7.getPoint())));
+
+        P6.addSegment(new GraphSegment(CHarbor, (int) P6.getPoint().distance(CHarbor.getPoint())));
+        P6.addSegment(new GraphSegment(P5, (int) P6.getPoint().distance(P5.getPoint())));
+        P6.addSegment(new GraphSegment(P6, (int) P6.getPoint().distance(P6.getPoint())));
+
+        P7.addSegment(new GraphSegment(DHarbor, (int) P7.getPoint().distance(DHarbor.getPoint())));
+        P7.addSegment(new GraphSegment(P5, (int) P7.getPoint().distance(P5.getPoint())));
+        P7.addSegment(new GraphSegment(P6, (int) P7.getPoint().distance(P6.getPoint())));
+
+        int temp = 30;
+        Harbor harborA = new Harbor("harborA","",new Point((65-temp)*GameConfiguration.GAME_SCALE,(80-temp)*GameConfiguration.GAME_SCALE),AHarbor);
+        Harbor harborB = new Harbor("harborA","",new Point((150-temp)*GameConfiguration.GAME_SCALE,(300-temp)*GameConfiguration.GAME_SCALE),BHarbor);
+        Harbor harborC = new Harbor("harborA","",new Point((340-temp)*GameConfiguration.GAME_SCALE,(130-temp)*GameConfiguration.GAME_SCALE),CHarbor);
+        Harbor harborD = new Harbor("harborA","",new Point((580-temp)*GameConfiguration.GAME_SCALE,(225-temp)*GameConfiguration.GAME_SCALE),DHarbor);
+
+        Faction faction1 = new Faction("red");
+
+
+        player.addHarbor(harborA);
+        faction1.addHarbor(harborD);
+
+        lstBotFaction.add(faction1);
+        lstHarbor.add(harborA);
+        lstHarbor.add(harborB);
+        lstHarbor.add(harborC);
+        lstHarbor.add(harborD);
 
         lstFaction.addAll(lstBotFaction);
         lstFaction.addLast(player);
--- src/main/java/maritime/test/TestMove.java
+++ src/main/java/maritime/test/TestMove.java
@@ -2,42 +2,18 @@ package maritime.test;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
-
 import maritime.config.GameConfiguration;
 import maritime.config.MapBuilder;
-import maritime.engine.entity.Harbor;
 import maritime.engine.entity.boats.Fodder;
 import maritime.engine.entity.boats.Merchant;
 import maritime.engine.entity.boats.Military;
 import maritime.engine.entity.boats.Standard;
-import maritime.engine.faction.Faction;
-import maritime.engine.faction.Player;
-import maritime.engine.graph.*;
-import maritime.engine.process.BoatManager;
 import maritime.engine.process.FactionManager;
-import maritime.engine.process.PlayerManager;
 import maritime.gui.GameDisplay;
 
 public class TestMove extends JFrame implements Runnable {
-
-    private GraphPoint F = new GraphPoint(new Point(490*GameConfiguration.GAME_SCALE,70*GameConfiguration.GAME_SCALE),"F");
-
-    private Harbor harbor1 = new Harbor("bob","red",new Point(490*GameConfiguration.GAME_SCALE,30*GameConfiguration.GAME_SCALE),F);
-
-    private Military military = new Military("bob","red",new Point(10,10));
-    private Standard standard2 = new Standard("carl","blue",new Point(10,10));
-
-    private Standard standard = new Standard("bob","red",new Point(10,10));
-    private Military military2 = new Military("bob","blue",new Point(10,10));
-
-    private Fodder fodder = new Fodder("bob","",new Point(10,10));
-    private Merchant merchant2 = new Merchant("bob","red",new Point(10,10));
-
     private MapBuilder map = new MapBuilder(0);
-    private PlayerManager playerManager = new PlayerManager(map);
     private FactionManager factionManager = new FactionManager(map);
-    private BoatManager boatManager = new BoatManager(map);
 
     private GameDisplay dashboard;
 
@@ -47,101 +23,35 @@ public class TestMove extends JFrame implements Runnable {
     }
 
     private void init() {
-        Player player = new Player("blue");
-        Faction faction = new Faction("");
-        player.addBoat(military);
-        player.addBoat(standard2);
-
-        player.addBoat(standard);
-        player.addBoat(military2);
-
-        faction.addBoat(fodder);
-        player.addBoat(merchant2);
-
-        ArrayList<Faction> lstBotFaction = new ArrayList<>();
-        ArrayList<Faction> lstFaction = new ArrayList<>();
-        lstBotFaction.add(faction);
-        ArrayList<Harbor> lstHarbor = new ArrayList<>();
-        lstHarbor.add(harbor1);
-        player.addHarbor(harbor1);
-        map.setLstBotFaction(lstBotFaction);
-        map.setPlayer(player);
-        map.setLstHarbor(lstHarbor);
-
-        lstFaction.addAll(lstBotFaction);
-        lstFaction.add(player);
-
-        map.setLstFaction(lstFaction);
-
         dashboard = new GameDisplay(map);
 
+        Military military0 = new Military("military0","blue",new Point(320*GameConfiguration.GAME_SCALE,180*GameConfiguration.GAME_SCALE));
+        Military military1 = new Military("military1","blue",new Point(320*GameConfiguration.GAME_SCALE,180*GameConfiguration.GAME_SCALE));
+        Merchant merchant0 = new Merchant("merchant0","blue",new Point(320*GameConfiguration.GAME_SCALE,180*GameConfiguration.GAME_SCALE));
+        Standard standard0 = new Standard("standard0","blue",new Point(320*GameConfiguration.GAME_SCALE,180*GameConfiguration.GAME_SCALE));
+        Standard standard1 = new Standard("standard1","blue",new Point(320*GameConfiguration.GAME_SCALE,180*GameConfiguration.GAME_SCALE));
+        Fodder fodder0 = new Fodder("fodder0","blue",new Point(320*GameConfiguration.GAME_SCALE,180*GameConfiguration.GAME_SCALE));
 
-        GraphPoint A = new GraphPoint(new Point(20*GameConfiguration.GAME_SCALE,80*GameConfiguration.GAME_SCALE),"A");
-        GraphPoint B = new GraphPoint(new Point(160*GameConfiguration.GAME_SCALE,160*GameConfiguration.GAME_SCALE),"B");
-        GraphPoint C = new GraphPoint(new Point(320*GameConfiguration.GAME_SCALE,320*GameConfiguration.GAME_SCALE),"C");
-        GraphPoint D = new GraphPoint(new Point(440*GameConfiguration.GAME_SCALE,280*GameConfiguration.GAME_SCALE),"D");
-        GraphPoint G = new GraphPoint(new Point(520*GameConfiguration.GAME_SCALE,240*GameConfiguration.GAME_SCALE),"G");
-        GraphPoint E = new GraphPoint(new Point(560*GameConfiguration.GAME_SCALE,240*GameConfiguration.GAME_SCALE),"E");
+        map.getPlayer().getLstBoat().add(military0);
+        map.getPlayer().getLstBoat().add(military1);
+        map.getPlayer().getLstBoat().add(merchant0);
+        map.getPlayer().getLstBoat().add(standard0);
+        map.getPlayer().getLstBoat().add(standard1);
+        map.getPlayer().getLstBoat().add(fodder0);
 
-        A.addSegment(new GraphSegment(B,1));
+        Military military2 = new Military("military2","red",new Point(340*GameConfiguration.GAME_SCALE,180*GameConfiguration.GAME_SCALE));
+        Merchant merchant1 = new Merchant("merchant1","red",new Point(340*GameConfiguration.GAME_SCALE,180*GameConfiguration.GAME_SCALE));
+        Merchant merchant2 = new Merchant("merchant2","red",new Point(340*GameConfiguration.GAME_SCALE,180*GameConfiguration.GAME_SCALE));
+        Standard standard2 = new Standard("standard2","red",new Point(340*GameConfiguration.GAME_SCALE,180*GameConfiguration.GAME_SCALE));
+        Fodder fodder1 = new Fodder("fodder1","red",new Point(340*GameConfiguration.GAME_SCALE,180*GameConfiguration.GAME_SCALE));
+        Fodder fodder2 = new Fodder("fodder2","red",new Point(340*GameConfiguration.GAME_SCALE,180*GameConfiguration.GAME_SCALE));
 
-        B.addSegment(new GraphSegment(A,1));
-        B.addSegment(new GraphSegment(C,1));
-        B.addSegment(new GraphSegment(D,1));
-        B.addSegment(new GraphSegment(F,1));
-
-        C.addSegment(new GraphSegment(B,1));
-        C.addSegment(new GraphSegment(D,1));
-
-        D.addSegment(new GraphSegment(F,1));
-        D.addSegment(new GraphSegment(G,1));
-        D.addSegment(new GraphSegment(B,1));
-        D.addSegment(new GraphSegment(C,1));
-
-        G.addSegment(new GraphSegment(D,1));
-        G.addSegment(new GraphSegment(F,1));
-        G.addSegment(new GraphSegment(E,1));
-
-        F.addSegment(new GraphSegment(B,1));
-        F.addSegment(new GraphSegment(D,1));
-        F.addSegment(new GraphSegment(G,1));
-
-        E.addSegment(new GraphSegment(G,1));
-
-
-        ArrayList<GraphPoint> path = new ArrayList<GraphPoint>();
-        path = SearchInGraph.findPath(A,E);
-
-        ArrayList<GraphPoint> path4 = new ArrayList<GraphPoint>();
-        path4 = SearchInGraph.findPath(A,E);
-
-        ArrayList<GraphPoint> path3 = new ArrayList<GraphPoint>();
-        path3 = SearchInGraph.findPath(F,C);
-
-
-        military.setPath(path);
-        military.setPosition(new Point(A.getPoint()));
-        military.setContinuePath(false);
-
-        standard2.setPosition(new Point(E.getPoint()));
-
-        standard.setPath(path3);
-        standard.setPosition(new Point(G.getPoint()));
-        standard.setContinuePath(true);
-
-        military2.setPath(path3);
-        military2.setPosition(new Point(D.getPoint()));
-        military2.setContinuePath(true);
-
-        fodder.setPath(path4);
-        fodder.setPosition(new Point(C.getPoint()));
-        fodder.setContinuePath(true);
-
-        merchant2.setPath(path4);
-        merchant2.setPosition(new Point(F.getPoint()));
-        merchant2.setContinuePath(true);
-
-        factionManager.chaseBoat(standard2,military);
+        map.getLstFaction().getFirst().getLstBoat().add(military2);
+        map.getLstFaction().getFirst().getLstBoat().add(merchant1);
+        map.getLstFaction().getFirst().getLstBoat().add(merchant2);
+        map.getLstFaction().getFirst().getLstBoat().add(standard2);
+        map.getLstFaction().getFirst().getLstBoat().add(fodder1);
+        map.getLstFaction().getFirst().getLstBoat().add(fodder2);
 
         Container contentPane = getContentPane();
         contentPane.add(dashboard);
