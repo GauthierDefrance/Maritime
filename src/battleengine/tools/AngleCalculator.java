f9fabf6e9a72d0471db749e3121de878ff951198
GauthierDefrance <defrance.gauthier@gmail.com>
2025-03-16 16:26:02 +0100

    Update AngleCalculator.java

--- src/battleengine/tools/AngleCalculator.java
+++ src/battleengine/tools/AngleCalculator.java
@@ -5,25 +5,41 @@ import java.awt.Point;
 
 public final class AngleCalculator {
 
-    public static double calculateAngle(Point pointA, Point pointB){
-        double x1 = pointA.getX();
-        double y1 = pointA.getY();
-        double x2 = pointB.getX();
-        double y2 = pointB.getY();
+
+    /**
+     * Take the boat Origin and rotate it in the point of the point Direction
+     * @param origin
+     * @param direction
+     */
+    public static void rotateBoatCalculatedAngle(Boat origin, Point direction){
+        origin.setAngle(calculateAngle(origin, direction));
+    }
+
+    /**
+     * Angle PointB to PointA
+     * @param direction
+     * @param origin
+     * @return
+     */
+    public static double calculateAngle(Point origin, Point direction){
+        double x1 = direction.getX();
+        double y1 = direction.getY();
+        double x2 = origin.getX();
+        double y2 = origin.getY();
         //Angle in RADIAN
         return Math.atan2(y1 - y2, x1 - x2);
     }
 
-    public static double calculateAngle(Boat pointA, Point pointB) {
-        return calculateAngle(pointA.getPosition(), pointB);
+    public static double calculateAngle(Boat origin, Point direction) {
+        return calculateAngle(origin.getPosition(), direction);
     }
 
-    public static double calculateAngle(Point pointA, Boat pointB) {
-        return calculateAngle(pointA, pointB.getPosition());
+    public static double calculateAngle(Point origin, Boat direction) {
+        return calculateAngle(origin, direction.getPosition());
     }
 
-    public static double calculateAngle(Boat pointA, Boat pointB) {
-        return calculateAngle(pointA.getPosition(), pointB.getPosition());
+    public static double calculateAngle(Boat origin, Boat direction) {
+        return calculateAngle(origin.getPosition(), direction.getPosition());
     }
 
 }
