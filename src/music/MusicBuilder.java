79e6ccb9365f5474b5737b3fae6b50f39c416229
GauthierDefrance <defrance.gauthier@gmail.com>
2025-04-25 13:20:57 +0200

    Update MusicBuilder.java

--- src/music/MusicBuilder.java
+++ src/music/MusicBuilder.java
@@ -1,4 +1,58 @@
 package music;
 
+
+import java.io.File;
+
+import static config.GameConfiguration.MUSIC_FILE_PATH;
+
+
+/**
+ * This class serve the purpose of building MusicPlayer that will be then given to the MusicManager.
+ * @author Gauthier Defrance
+ * Class MusicBuilder
+ * @version 0.1
+ */
 public class MusicBuilder {
+
+
+    /**
+     * Method that create an object MusicPlayer with a specific name like : my_music.wav
+     * @param filename the full name with extension of the file
+     * @return {@link MusicPlayer} object that can play music and must be stopped
+     */
+    public MusicPlayer createMusic(String filename) {
+        File fichier = new File(MUSIC_FILE_PATH+filename);
+        if(fichier.exists()) {
+            MusicPlayer mp = new MusicPlayer();
+            mp.load(MUSIC_FILE_PATH+filename);
+            return mp;
+        } else {
+            //ERREUR
+            return null;
+        }
+    }
+
+    /**
+     * Method that create an object MusicPlayer with a specific name like : my_music.wav
+     * @param filename the full name with extension of the file
+     * @param loop {@link Boolean} that indicate if the music player should be initialized in looping mode.
+     * @return {@link MusicPlayer} object that can play music and must be stopped
+     */
+    public MusicPlayer createMusic(String filename, Boolean loop) {
+        File fichier = new File(MUSIC_FILE_PATH+filename);
+        if(fichier.exists()) {
+            MusicPlayer mp = new MusicPlayer();
+            mp.load(MUSIC_FILE_PATH+filename);
+            if(loop) {
+                mp.loop();
+            }
+            return mp;
+        } else {
+            //ERREUR
+            return null;
+        }
+    }
+
+
+
 }
